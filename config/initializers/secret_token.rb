e04c5738a7a9048b1327d110793de44a1ea9bc28
ECN-Energy <soogunfowora@gmail.com>
2016-02-05 15:22:40 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'c52eade5646a5de12feddb3a4cb04326723a2a6b97d4a7a541b7b6c8846c5f7fa1f2a1c9f4bfa5fe151992ac8cf26507f662acf292eefecc3d2879e55dc03e20'
